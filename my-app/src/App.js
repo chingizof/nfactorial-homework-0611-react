2405498fa3e42fed3e0d0ad1ccd97c6e44b3daf3
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-06-13 13:53:33 +0600

    created adaptive design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,29 @@
+import './App.css';
+
 function App() {
   return (
     <>
-      <div
-        style = {{
-          float: "left",
-          width: "20%",
-        }}>
-        <img src="https://source.unsplash.com/gYl-UtwNg_I/1500x1500"/>
-      </div>
-      <div
-        style = {{
-          float: "left",
-          width: "20%",
-        }}>
-        <img src="https://source.unsplash.com/rFKUFzjPYiQ/1500x1500"/>
-      </div>
-      <div
-        style = {{
-          float: "left",
-          width: "20%",
-        }}>
-        <img src="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d"/>
-      </div>
-      <div
-        style = {{
-          float: "left",
-          width: "20%",
-        }}>
-        <img src="https://source.unsplash.com/ITjiVXcwVng/1500x1500"/>
-      </div>
-      <div
-        style = {{
-          float: "left",
-          width: "20%",
-        }}>
-        <img src="https://source.unsplash.com/3MNzGlQM7qs/1500x1500"/>
+      <div className="image-container">
+        <div className="column">
+          <img src="https://source.unsplash.com/gYl-UtwNg_I/1500x1500"/>
+          <div className="image-text">HEY</div>
+        </div>
+        <div className="column">
+          <img src="https://source.unsplash.com/rFKUFzjPYiQ/1500x1500"/>
+          <div className="image-text">LET'S</div>
+        </div>
+        <div className="column">
+          <img src="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d"/>
+          <div className="image-text">GIVE</div>
+        </div>
+        <div className="column">
+          <img src="https://source.unsplash.com/ITjiVXcwVng/1500x1500"/>
+          <div className="image-text">ALL</div>
+        </div>
+        <div className="column">
+          <img src="https://source.unsplash.com/3MNzGlQM7qs/1500x1500"/>
+          <div className="image-text">YOU CAN</div>
+        </div>
       </div>
     </>
   );
