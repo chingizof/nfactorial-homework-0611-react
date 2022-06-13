07e6200d1ff3531ab61110e1a4f78832ca7169f2
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-06-13 12:40:14 +0600

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,43 @@
+function App() {
+  return (
+    <>
+      <div
+        style = {{
+          float: "left",
+          width: "20%",
+        }}>
+        <img src="https://source.unsplash.com/gYl-UtwNg_I/1500x1500"/>
+      </div>
+      <div
+        style = {{
+          float: "left",
+          width: "20%",
+        }}>
+        <img src="https://source.unsplash.com/rFKUFzjPYiQ/1500x1500"/>
+      </div>
+      <div
+        style = {{
+          float: "left",
+          width: "20%",
+        }}>
+        <img src="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d"/>
+      </div>
+      <div
+        style = {{
+          float: "left",
+          width: "20%",
+        }}>
+        <img src="https://source.unsplash.com/ITjiVXcwVng/1500x1500"/>
+      </div>
+      <div
+        style = {{
+          float: "left",
+          width: "20%",
+        }}>
+        <img src="https://source.unsplash.com/3MNzGlQM7qs/1500x1500"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
